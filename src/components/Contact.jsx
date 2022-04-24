6b92be7f23a16f6d08b7c5e51e10a4f35212e8e7
astrokachi <astrokachi@gmail.com>
2022-04-24 03:35:58 +0100

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Contact.css";
-import Socials from "./Socials";
+
 
 const Contact = () => {
 	return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { IoIosRocket } from "react-icons/io";
 import "./Header.css";
 
