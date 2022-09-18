9d897bd2ddc406421c22472d5ee1833644ac434e
Kachi <astrokachi@gmail.com>
2022-09-18 20:16:52 +0100

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,37 +1,36 @@
-import React from "react";
-import "./Hero.css";
-import { BsTools } from "react-icons/bs";
-import Fade from "react-reveal/Fade";
+import React from 'react';
+import './Hero.css';
+import { BsTools } from 'react-icons/bs';
+import Fade from 'react-reveal/Fade';
 
 const Hero = () => {
-	return (
-		<Fade bottom>
-			<div className="hero">
-				<h2 className="hero__text1">Hi, my name is</h2>
-				<h1 className="hero__name">Kachi Ozoemena.</h1>
-				<h2 className="hero__support">
-					I build things for the web.
-					<span>
-						{" "}
-						<BsTools />
-					</span>
-				</h2>
-				<h4 className="hero__description">
-					I'm a software engineer specializing in building exceptional digital
-					experiences. Currently, I'm focused on building accessible, user
-					friendly products.{" "}
-				</h4>
-				<a
-					className="hero__a"
-					href="https://drive.google.com/file/d/1i78_P7YdXNlor2BDauJwKmf2qI1m-M3F/view?usp=sharing"
-					target={"_blank"}
-					rel="noreferrer"
-				>
-					<button className="hero__resumeButton">Resume</button>
-				</a>
-			</div>
-		</Fade>
-	);
+  return (
+    <Fade bottom>
+      <div className="hero">
+        <h2 className="hero__text1">Hi, my name is</h2>
+        <h1 className="hero__name">Kachi Ozoemena.</h1>
+        <h2 className="hero__support">
+          I build things for the web.
+          <span>
+            {' '}
+            <BsTools />
+          </span>
+        </h2>
+        <h4 className="hero__description">
+          I'm a software engineer specializing in building exceptional digital experiences. Currently, I'm focused on
+          building accessible, user friendly products.{' '}
+        </h4>
+        <a
+          className="hero__a"
+          href="https://drive.google.com/file/d/1i78_P7YdXNlor2BDauJwKmf2qI1m-M3F/view?usp=sharing"
+          target={'_blank'}
+          rel="noreferrer"
+        >
+          <button className="hero__resumeButton">Resume</button>
+        </a>
+      </div>
+    </Fade>
+  );
 };
 
 export default Hero;
